ecaa4fc6aca292098c1a52fff48ef61df08508ad
jose <joseco04@live.com>
2020-08-27 02:55:02 +0300

    client validation done

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,30 +5,40 @@ const valuesX = ['-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3'];
 const valuesR = ['1', '1.5', '2', '2.5', '3'];
 for (const number of valuesX) {
     $('#options_x')
-        .append(`<input type="checkbox" id="${number}" name="x[]" value="${number}">`)
+        .append(`<input type="checkbox" id="${number}" name="x[]" value="${number}" form="request-form">`)
         .append(`<label for="${number}">${number}</label>`);
 }
 for (const number of valuesR) {
     $('#options_r')
-        .append(`<input type="radio" id="${number}" name="r" value="${number}">`)
+        .append(`<input type="radio" id="${number}" name="r" value="${number}" form="request-form" ${(number==='1') ? 'checked' : ''}>`)
         .append(`<label for="${number}">${number}</label>`);
 }
 
 
 /*
 * validate input
-* TODO: Validation
+* TODO: CORRECT WHEN THERE IS TEXT AFTER THE NUMBER
 * */
 function validateY(input) {
-    let y = parseFloat(input.value.replace(',', '.'));
-    return (isNaN(input) && (y >= -5 && y <= 3));
+    let val = parseFloat(input.replace(',','.'));
+    if (isNaN(val))
+        return false;
+
+    return val >= -5 && val <= 3;
 }
 
 function validateInput() {
-    var validX, validY, validR = true;
-    validY = validateY($('input#y')[0]);
-    console.log(validY)
-    $('#submit-btn')[0].disabled = validX && validY && validR;
+    var validX, validY, validR;
+    validY = validateY($('input#y').val());
+
+    validX = $('input:checkbox').filter(':checked').length > 0;
+    validR = $('input:radio').filter(':checked').length === 1;
+
+    console.log('validX=' + validX + ', validY=' + validR + ', validR=' + validR);
+
+        $('#submit-btn').attr('disabled', !(validX && validY && validR));
+
+    return validX && validY && validR;
 }
 
 
@@ -37,15 +47,20 @@ function validateInput() {
 * */
 $('#request-form').submit(function (event) {
     event.preventDefault();
+    if (!validateInput()) {
+        alert('at least one checkbox selected, the radio selected and a nice number for Y');
+        return;
+    }
 
     let action = "handler.php";
     let data = $(this).serialize();
 
     $.post(action, data, function (response) {
-        if (response.RESULT_CODE === 1) {
+        if (response.RESULT_CODE === 0) {
             addResultRow(response.PROPS);
         } else {
             alert(response.PROPS);
+            console.log(response.PROPS);
         }
     });
 });
@@ -66,7 +81,6 @@ function addResultRow(response) {
         "</tr>"
     );
 }
-
 
 $(window).resize(drawCanvas)
 $(window).on("load", drawCanvas)
--- handler.php
+++ handler.php
@@ -5,10 +5,25 @@ $y = $_POST['y'];
 $r = $_POST['r'];
 header('Content-type: application/json');
 
+$argsXandY = array('options' => array('min_range' => -5, 'max_range' => 3));
+$argsR = array('options' => array('min_range' => 1, 'max_range' => 3));
+
+$validatedX = true;
+foreach ($x as $val) {
+    if (filter_var($val, FILTER_VALIDATE_FLOAT, $argsXandY) === FALSE) {
+        $validatedX = false;
+        break;
+    }
+}
+
+if (!$validatedX or filter_var($y, FILTER_VALIDATE_FLOAT, $argsXandY) === FALSE or filter_var($r, FILTER_VALIDATE_FLOAT, $argsR) === FALSE) {
+    echo json_encode(array('RESULT_CODE' => 1, 'PROPS' => 'What are u trying to do??? don\'t joke with my validation'));
+    die();
+}
 
 
 $response = json_encode(
-    array('RESULT_CODE' => 1,
+    array('RESULT_CODE' => 0,
           'PROPS' =>
               array('x' => $x,
                   'y' => $y,
